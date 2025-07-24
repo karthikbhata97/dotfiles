c752d722aa5b5492019ad94817e7b6b099f8afe1
Karthik Bhat A <karthik.bhata7@gmail.com>
2025-07-24 10:42:23 +0530

    Minor changes

--- nvim/.config/nvim/lua/an0ne/set.lua
+++ nvim/.config/nvim/lua/an0ne/set.lua
@@ -25,6 +25,10 @@ vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>w', '<C-w>w', { noremap = true, silent = true }) -- Cycle to the next split
+vim.keymap.set('n', '<leader>v', '<C-w>v', { noremap = true, silent = true }) -- Vertical split
+vim.keymap.set('n', '<leader>s', '<C-w>s', { noremap = true, silent = true }) -- Horizontal split
+vim.keymap.set('t', '<C-[>', '<C-\\><C-n>', { noremap = true, silent = true }) -- Exit terminal mode
+
 
 vim.keymap.set('n', '<leader><S-z>', function ()
     vim.cmd("wincmd =")
