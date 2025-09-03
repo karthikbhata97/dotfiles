991ca382cc5300856d52d9cae7b358b35850f193
Karthik Bhat A <karthik.bhata7@gmail.com>
2025-09-03 09:48:20 -0400

    Minor fixes

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,9 @@ require("telescope").setup({
     pickers = {
         buffers = {
             initial_mode = "normal",
+        },
+        defaults = {
+            sorting_strategy = "ascending",
         }
     }
 })
@@ -9,7 +12,7 @@ require("telescope").setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', function() builtin.buffers({sort_lastused = true}) end, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fb', function() builtin.buffers({sort_mru = true}) end, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader><Tab>', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope grep word under cursor' })
