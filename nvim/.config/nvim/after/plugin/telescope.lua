eb8920a919f21841c95c2c03d9d967904c230579
Karthik Bhat A <karthik.bhata7@gmail.com>
2025-09-25 17:22:35 -0400

    Update telescope buffers

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -13,7 +13,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', function() builtin.buffers({sort_mru = true}) end, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader><Tab>', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader><Tab>', function() builtin.buffers({sort_mru = true, ignore_current_buffer = true}) end, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope grep word under cursor' })
 
