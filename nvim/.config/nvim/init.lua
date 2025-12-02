1d6b25ab336b837ee86dd24bbcce62e29836abbe
Karthik Bhat A <karthik.bhata7@gmail.com>
2025-12-02 15:45:25 -0500

    Update jumping behavior for windows

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,28 +1,18 @@
 vim.g.mapleader = ","
 vim.opt.exrc = true
 
-local prev_window = vim.api.nvim_get_current_win()
-function GotoPrevWindow()
-    local current_window = vim.api.nvim_get_current_win()
-    if prev_window == current_window then
-        local all_windows = vim.api.nvim_list_wins()
-        local total_windows = #all_windows
-        for i, win in ipairs(all_windows) do
-            if win == current_window then
-                if i == 1 then
-                    prev_window = all_windows[total_windows]
-                else
-                    prev_window = all_windows[i - 1]
-                end
-            end
-        end
-    end
+local prev_window = nil
 
-    vim.api.nvim_set_current_win(prev_window)
-    prev_window = current_window
+function GotoPrevWindow()
+    local curr_window = vim.api.nvim_get_current_win()
+    if prev_window and vim.api.nvim_win_is_valid(prev_window) and prev_window ~= curr_window then
+        vim.api.nvim_set_current_win(prev_window)
+    else
+        vim.cmd('wincmd w')
+    end
 end
 
-vim.api.nvim_create_autocmd({"WinEnter"}, {
+vim.api.nvim_create_autocmd("WinLeave", {
     callback = function()
         prev_window = vim.api.nvim_get_current_win()
     end
